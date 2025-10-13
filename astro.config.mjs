32099748ca2a73bb0815e740777a11f5890a7123
Theyobii <202856387+Theyobii@users.noreply.github.com>
2025-10-13 00:14:50 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,50 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind"
+
+// For alias imports
+import { fileURLToPath, URL } from "node:url"
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        react(),
+        tailwind(),
+    ],
+    vite: {
+        plugins: [],
+        resolve: {
+            alias: {
+                "@styles": fileURLToPath(
+                    new URL(
+                        "./src/styles",
+                        import.meta.url
+                    )
+                ),
+                "@assets": fileURLToPath(
+                    new URL(
+                        "./src/assets",
+                        import.meta.url
+                    )
+                ),
+                "@components": fileURLToPath(
+                    new URL(
+                        "./src/components",
+                        import.meta.url
+                    )
+                ),
+            },
+        },
+    },
+
+    output: "static",
+    build: {
+        inlineStylesheets: "auto",
+    },
+    server: {
+        host: true,
+        port: 4321,
+    },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,astro}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: [
+                    "inter",
+                ],
+            },
+        },
+    },
+    plugins: [],
+}
+
