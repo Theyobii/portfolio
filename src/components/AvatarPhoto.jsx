cdd29d84f19098fc59c051215bca1759be6c4c60
Theyobii <202856387+Theyobii@users.noreply.github.com>
2025-10-13 23:22:56 -0400

    fix: photo profile

---
+++ src/components/AvatarPhoto.jsx
@@ -0,0 +1,8 @@
+import Avatar from "../assets/yo.jpg"
+import React from 'react';
+
+export const AvatarPhoto = ({ className = "" }) => {
+    return <img src={Avatar.src} alt="jonny" className={className} />
+}
+
+export default AvatarPhoto;
