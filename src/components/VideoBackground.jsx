d2db7b47881ce54e3a4a5d1d673ca9c0720e2c3e
Theyobii <202856387+Theyobii@users.noreply.github.com>
2025-11-06 22:47:21 -0400

    fix: edit the background & footer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const VideoBackground = ({ src, children }) => {
   return (
-    <div className="relative w-full h-screen overflow-hidden flex justify-center items-center">
+    <div className="absolute inset-0 -z-10 overflow-hidden">
       <video
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full w-auto h-auto object-cover z-[-1]"
+        className="w-full h-full object-cover"
         autoPlay
         loop
         muted
@@ -14,7 +14,7 @@ const VideoBackground = ({ src, children }) => {
         {<source src={src} type="./public/background.mp4" />}
         Tu navegador no soporta el tag de video.
       </video>
-      <div className="relative z-10 text-white text-center p-5">
+      <div className="relative z-10 ">
         {children}
       </div>
     </div>
