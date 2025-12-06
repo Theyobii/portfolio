9e3e1aabc0c320488315af9f17f55d1008ec89f6
ImMau14 <newmauuelenoski02@gmail.com>
2025-12-06 01:53:28 -0400

    Feat: Add @astrojs/vercel integration for headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,45 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config'
+import { defineConfig, envField } from 'astro/config';
 
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // For alias imports
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-// https://astro.build/config
 export default defineConfig({
+  experimental: {
+    csp: true,
+  },
+
+  adapter: vercel({
+    experimentalStaticHeaders: true
+  }),
+
   integrations: [react(), tailwind()],
+
   vite: {
     resolve: {
       alias: {
         '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
         '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      },
-    },
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url))
+      }
+    }
   },
 
-  output: 'static',
-  build: { inlineStylesheets: 'auto' },
+  output: 'server',
 
   env: {
     schema: {
-      FOOTBALL_API_KEY: envField.string({ context: 'server', access: 'secret' }),
+      FOOTBALL_API_KEY: envField.string({ context: 'server', access: 'secret' })
     },
-    validateSecrets: false,
+    validateSecrets: false
   },
 
   server: {
     host: true,
-    port: 4321,
-  },
-})
+    port: 4321
+  }
+});
